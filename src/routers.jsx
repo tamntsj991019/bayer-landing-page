f05d4724f892725110da976709d418201ca32b29
TamNT <tamnt.sj.991019@gmail.com>
2024-02-28 23:35:12 +0700

    update

--- src/routers.jsx
+++ src/routers.jsx
@@ -1,9 +1,7 @@
 import { Home } from "./screens";
-import Page2 from "./screens/page2";
 
 export const EPath = {
   Home: "/",
-  P2: "/p2",
 };
 
 export const AppRoutes = [
@@ -13,14 +11,6 @@ export const AppRoutes = [
     path: EPath.Home,
     label: "Trang chủ",
     element: <Home />,
-    visible: false,
-  },
-
-  {
-    key: EPath.P2,
-    path: EPath.P2,
-    label: "P 2",
-    element: <Page2 />,
     visible: false,
   },
 ];
