afe31332f45a94052e11392d54c5fe9ea2138b7d
TamNT <tamnt.sj.991019@gmail.com>
2024-03-06 17:14:27 +0700

    update

--- src/screens/home/paths/UserManual.jsx
+++ src/screens/home/paths/UserManual.jsx
@@ -24,10 +24,15 @@ const UserManual = (props) => {
 export default UserManual;
 
 const TableGuild = () => {
-  const cssHeader = "h-[90px] flex items-center justify-center p-0";
+  const cssHeader =
+    "h-[90px] flex items-center justify-center p-0 ps-2 border-0";
   const clipPathStyle = {
     clipPath: "polygon(15% 0%, 100% 0, 100% 100%, 0% 100%)",
   };
+
+  const cssTd = "td-top text-center h-[400px]";
+  const cssTdEven = `${cssTd} bg-[#DDD2DB]`;
+  const cssTdOdd = `${cssTd} bg-[#F0EDF0]`;
   return (
     <table className="w-[1150px]">
       <thead className="h-[90px] text-white">
@@ -43,9 +48,9 @@ const TableGuild = () => {
             </div>
           </th>
           <th className="w-[187px] p-0">
-            <div className="bg-[#2B6638]">
+            <div className=" bg-[#2B6638] ">
               <div
-                className={`${cssHeader} bg-[#67B514]`}
+                className={`${cssHeader} bg-[#67B514] `}
                 style={{ ...clipPathStyle, zIndex: 2 }}
               >
                 Đối tượng
@@ -98,49 +103,109 @@ const TableGuild = () => {
                 className={`${cssHeader} bg-[#481B38]`}
                 style={{ zIndex: 7 }}
               >
-                Mức nước và thời gian giữ nước
+                Mức nước và <br /> thời gian giữ nước
               </div>
             </div>
           </th>
         </tr>
       </thead>
-      <tbody className="">
+      <tbody>
         <tr>
-          <td className="bg-[#DDD2DB] text-center">
-            <div className="">
+          <td className={`${cssTdEven}`}>
+            <div>
               <span>Lúa sạ thẳng</span>
             </div>
           </td>
-          <td className="bg-[#F0EDF0] text-center">
-            1. Nhóm chác lác: chác (lông heo); cỏ cháo (lác rận); u du (lác
-            cơm); lác. 2. Nhóm hoà bản: lồng vực (cỏ gạo); đuôi phụng (lông
-            công, cỏ bay). 3. Nhóm lá rộng: rau mương; mác bao; cỏ chân vịt; rau
-            bợ; cỏ xà bông...
+          <td className={`${cssTdOdd}`}>
+            <div className="flex flex-col text-start">
+              <span>
+                1. Nhóm chác lác: chác (lông heo); cỏ cháo (lác rận); u du (lác
+                cơm); lác.
+              </span>
+              <span>
+                2. Nhóm hoà bản: lồng vực (cỏ gạo); đuôi phụng (lông công, cỏ
+                bay).
+              </span>
+              <span>
+                3. Nhóm lá rộng: rau mương; mác bao; cỏ chân vịt; rau bợ; cỏ xà
+                bông...
+              </span>
+            </div>
           </td>
-          <td className="bg-[#DDD2DB] text-center">7-9 ngày sau sạ</td>
-          <td className="bg-[#F0EDF0] text-center">
-            0.25 Lít/ha. Không cần cộng thêm thuốc cỏ khác
+          <td className={`${cssTdEven}`}>7-9 ngày sau sạ</td>
+          <td className={`${cssTdOdd}`}>
+            <div className="text-start">
+              <div className="mb-8">0.25 Lít/ha.</div>
+
+              <div>Không cần cộng thêm thuốc cỏ khác</div>
+            </div>
           </td>
-          <td className="bg-[#DDD2DB] text-center">
-            <span>Phun thường. Đất bảo hoà nước</span>
-            <span>Phun toé. Đất có mực nước 2-3cm</span>
+          <td className={`${cssTdEven}`}>
+            <SplitTwo
+              alignStart={true}
+              css1="h-[200px] pt-[34px]"
+              child1={
+                <>
+                  <div>Phun thường.</div>
+                  <div>Đất bảo hoà nước</div>
+                </>
+              }
+              css2="h-[200px] pt-[20px]"
+              child2={
+                <>
+                  <div>Phun toé.</div>
+                  <div>Đất có mực nước 2-3cm</div>
+                </>
+              }
+            />
           </td>
-          <td className="bg-[#F0EDF0] text-center">
-            <span>400 lít/HA</span>
-            <span>125 lít/HA</span>
+          <td className={`${cssTdOdd}`}>
+            <SplitTwo
+              alignStart={true}
+              css1="h-[200px] pt-[34px]"
+              child1={<div>400 lít/HA</div>}
+              css2="h-[200px] pt-[20px]"
+              child2={<div>125 lít/HA</div>}
+            />
           </td>
-          <td className="bg-[#DDD2DB] text-center">
-            <span>
-              • Mặt ruộng bão hòa nước. • Sau khi phun từ 1 - 2 ngày thì đưa
-              nước vào và giữ nước theo tập quán của nông dân.
-            </span>
-            <span>
-              • Mặt ruộng có mức nước 2 - 3 cm. • Giữ nước từ 2 - 3 ngày sau
-              phun.
-            </span>
+          <td className={`${cssTdEven}`}>
+            <SplitTwo
+              alignStart={true}
+              css1="h-[250px] pt-[34px]"
+              child1={
+                <>
+                  <div>• Mặt ruộng bão hòa nước.</div>
+                  <div>
+                    • Sau khi phun từ 1 - 2 ngày thì đưa nước vào và giữ nước
+                    theo tập quán của nông dân.
+                  </div>
+                </>
+              }
+              css2="h-[145px] pt-[10px]"
+              child2={
+                <>
+                  <div>• Mặt ruộng có mức nước 2 - 3 cm.</div>
+                  <div>• Giữ nước từ 2 - 3 ngày sau phun.</div>
+                </>
+              }
+            />
           </td>
         </tr>
       </tbody>
     </table>
+  );
+};
+
+const SplitTwo = ({ child1, child2, css1, css2, alignStart }) => {
+  const wrapperCss = [
+    "-mx-[16px] -my-[34px] h-[100%]",
+    alignStart ? "text-start" : "",
+  ];
+  const childCss = `px-[16px] border-[#e3dee3]`;
+  return (
+    <div className={wrapperCss.join(" ")}>
+      <div className={`${childCss} border-b-2 ${css1}`}>{child1}</div>
+      <div className={`${childCss} border-t-2 ${css2}`}>{child2}</div>
+    </div>
   );
 };
